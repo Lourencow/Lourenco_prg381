6b9fafee962de8264052902896f0f5cb46e499e6
lourencowlov@gmail.com <lourencowlov@gmail.com>
2021-02-24 10:28:23 +0200

    yes

--- prg381.java
+++ prg381.java
@@ -3,6 +3,7 @@ public class prg381 {
     public static void main(String[] args){
         System.out.println("Help, I am beig held hostage by me");
         //this
+        //change
     }
     
 }
